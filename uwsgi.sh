e4ce43034479bd43d2ed8a33e1d4ebf842a24e93
Odd Eivind Ebbesen <odd@oddware.net>
2012-12-21 17:07:55 +0100

    Added uwsgi.sh

---
+++ uwsgi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+_base=$(dirname $(realpath $0))
+_pid="/tmp/dj-lhlunch.pid"
+_log="/tmp/django-lhlunch.log"
+_ip="0.0.0.0"
+_port="8001"
+
+
+echo uwsgi \
+   --chdir=$_base \
+   --socket="${_ip}:${_port}" \
+   --master \
+   --pidfile=$_pid \
+   --daemonize=$_log \
+   --processes=4 \
+   --uid=1000 \
+   --gid=33 \
+   --harakiri=20 \
+   --limit-as=128 \
+   --max-requests=5000 \
+   --vacuum \
+   --module=LHLunchWebGUI.wsgi:application \
+   --env DJANGO_SETTINGS_MODULE=LHLunchWebGUI.settings
