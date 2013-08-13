173b06278f36f7cdeae8cfaa9f6e9a5ea9f25fc1
Odd Eivind Ebbesen <odd@oddware.net>
2013-08-13 21:56:39 +0200

    Finally figured out how to start the app with the new uwsgi binary

--- LHLunchWebGUI/wsgi.py
+++ LHLunchWebGUI/wsgi.py
@@ -20,8 +20,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LHLunchWebGUI.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
+
+# Trying another way...
+# Odd, 2013-08-13 21:22:25
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
--- uwsgi.sh
+++ uwsgi.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-_base=$(dirname $(realpath $0))
-_pid="/tmp/dj-lhlunch.pid"
-_log="/tmp/django-lhlunch.log"
-_ip="0.0.0.0"
-_port="8001"
+#_base=$(dirname $(realpath $0))
+#_pid="/tmp/dj-lhlunch.pid"
+#_log="/tmp/django-lhlunch.log"
+#_ip="0.0.0.0"
+#_port="8001"
+#
+#
+#uwsgi \
+#   --chdir=$_base \
+#   --socket="${_ip}:${_port}" \
+#   --master \
+#   --pidfile=$_pid \
+#   --daemonize=$_log \
+#   --processes=4 \
+#   --max-requests=512 \
+#   --vacuum \
+#   --module=LHLunchWebGUI.wsgi:application \
+#   --env DJANGO_SETTINGS_MODULE=LHLunchWebGUI.settings
 
-
-uwsgi \
-   --chdir=$_base \
-   --socket="${_ip}:${_port}" \
-   --master \
-   --pidfile=$_pid \
-   --daemonize=$_log \
-   --processes=4 \
-   --max-requests=512 \
-   --vacuum \
-   --module=LHLunchWebGUI.wsgi:application \
-   --env DJANGO_SETTINGS_MODULE=LHLunchWebGUI.settings
+uwsgi --need-app --plugin python2 --ini ./uwsgi.ini
